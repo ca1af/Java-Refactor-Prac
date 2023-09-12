ef7b53b795a5693f8a78845daccc0b33839c91a3
DK <laancer4@gmail.com>
2023-09-12 16:54:34 +0900

    [FIX] IntelliJ Hints + Gradle 이슈 해결

---
+++ src/test/java/com/gildedrose/ItemTest.java
@@ -0,0 +1,17 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class ItemTest {
+    private static final String NAME = "DK";
+    private static final Integer SELL_IN = 5;
+    private static final Integer QUALITY = 10;
+    private Item item;
+    @BeforeEach
+    void initItem(){
+        item = new Item(NAME, SELL_IN, QUALITY);
+    }
+}
